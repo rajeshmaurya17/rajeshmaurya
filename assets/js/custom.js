ef3eeaa41e927fdaccc04da0371e9f26c1ea8b44
rajeshmaurya17 <87825735+rajeshmaurya17@users.noreply.github.com>
2024-08-29 10:47:12 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,3 +5,10 @@ if (pass == "mera@password") {
 } else {
   alert("The password you've is enter is incorrect");
 }
+
+
+  document.getElementById('input').addEventListener('keyup', function(event) {
+   if(document.getElementById('input').value == 3) {
+      window.open("aw3za.html")
+    }
+})
