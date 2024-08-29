65b504f1d9a9c6eeef89d40930355da61c3916ce
rajeshmaurya17 <87825735+rajeshmaurya17@users.noreply.github.com>
2024-08-29 09:21:22 +0530

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,5 +3,5 @@ pass = prompt("please enter the password to access");
 if (pass == "mera@password") {
   document.location.href = "myurl.html";
 } else {
-  document.write("The password you've is enter is incorrect");
+  alret("The password you've is enter is incorrect");
 }
